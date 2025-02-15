3704abfdbcc6e265725925c766373b47a5da43fd
ajeshphilip <66029815+ajeshphilip@users.noreply.github.com>
2025-02-15 04:59:09 +0000

    added the string file 2

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const  repoCount = 5
+const name = "Ajesh"
+
+console.log(`Hello my name is ${name} and my contribution to git is ${repoCount}`);
+const gameName = new String('hdfc-hitech-city-hyderabad')
+
+console.log(`gameName = ${gameName.length}`);
+console.log(`gameName char at 5 => ${gameName.charAt(5)}`)
+gameLength = gameName.length
+console.log(`gameName substring example => ${gameName.substring(5, gameLength)}`);
+console.log(`gameName slice example => ${gameName.slice(5, gameLength)}`)
+console.log(`index of 'y' in gameName variable => ${gameName.indexOf('y')}`)
